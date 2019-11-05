f047c86e5965e628d9374c4ddc7bca9bfa45229f
Jharell Chambers <jharell.chambers@selu.edu>
2019-11-05 22:10:54 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -99,7 +99,7 @@ class Student
       SELECT * FROM students WHERE grade = ? 
     SQL
 
-    DB[:conn].execute(sql).map do |row|
+    DB[:conn].execute(sql, int).map do |row|
       self.new_from_db(row)
     end
   end
