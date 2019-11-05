390e00c90ab74dbaca61d5f6f71da2c738dd27e4
Jharell Chambers <jharell.chambers@selu.edu>
2019-11-05 21:54:10 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -57,7 +57,7 @@ end
 
    def self.all_students_in_grade_9
     sql = <<-SQL
-      SELECT COUNT(*) FROM students WHERE grade =9;
+      SELECT COUNT(*) FROM students WHERE grade = 9;
     SQL
 
     DB[:conn].execute(sql).map do |row|
