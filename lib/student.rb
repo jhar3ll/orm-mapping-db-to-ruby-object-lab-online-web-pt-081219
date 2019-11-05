31b0a28962576ed56362443ef4cd717c360b62df
Jharell Chambers <jharell.chambers@selu.edu>
2019-11-05 22:01:56 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -64,7 +64,7 @@ class Student
     end
   end
   
-  def self.all_students_in_grade_11
+  def self.students_below_12th_grade
     sql = <<-SQL
       SELECT * FROM students WHERE grade < 12
     SQL
