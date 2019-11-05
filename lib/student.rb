4c0d4c71be5080f02f8698a9d496817dfe3830c3
Jharell Chambers <jharell.chambers@selu.edu>
2019-11-05 21:59:45 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -66,7 +66,7 @@ class Student
   
   def self.all_students_in_grade_9
     sql = <<-SQL
-      SELECT * FROM students WHERE grade <= 11
+      SELECT * FROM students WHERE grade <=
     SQL
 
     DB[:conn].execute(sql).map do |row|
