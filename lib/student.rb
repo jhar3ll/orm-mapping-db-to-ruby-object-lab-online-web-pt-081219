57f2a1ed89869171b5376556ba25cc6e8a837162
Jharell Chambers <jharell.chambers@selu.edu>
2019-11-05 22:07:27 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -86,7 +86,7 @@ class Student
   
   def self.first_student_in_grade_10
     sql = <<-SQL
-      SELECT * FROM students WHERE grade =10 
+      SELECT * FROM students WHERE grade =10 ORDER BY students.id LIMIT 1
     SQL
 
     DB[:conn].execute(sql).map do |row|
